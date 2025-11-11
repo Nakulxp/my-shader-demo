f1555e6c45a814719150e1d04c183c99360816f7
Nakul Ganga Prasad <nakulgprasad03@gmail.com>
2025-11-11 20:47:22 +0530

    FIX: Make shader a full-screen background

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-// app/page.tsx (or any other page/component file)
+// src/app/page.tsx
 import { DitheringShader } from "@/components/ui/dithering-shader";
 
 export default function DemoOne() {
   return (
-    <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden">
+    // 1. Make the container fill the screen and be the relative parent.
+    <div className="relative h-screen w-screen overflow-hidden">
+      
+      {/* 2. Make the shader the absolute background. 
+          We add className to stretch its div,
+          and we pass large width/height props for a good canvas resolution.
+      */}
       <DitheringShader
+        className="absolute inset-0 z-0 h-full w-full"
+        width={1920}   // Use a high-res default for the canvas
+        height={1080}  // This will be stretched/shrunk by the className
         shape="swirl"
         type="4x4"
         colorBack="#220011"
@@ -12,9 +21,14 @@ export default function DemoOne() {
         pxSize={4}
         speed={0.9}
       />
-      <span className="pointer-events-none z-10 text-center text-7xl leading-none absolute font-semibold text-white tracking-tighter whitespace-pre-wrap">
+      
+      {/* 3. The text is already absolute and z-10. 
+          We just need to center it in the relative parent.
+      */}
+      <span className="pointer-events-none z-10 text-center text-7xl leading-none absolute font-semibold text-white tracking-tighter whitespace-pre-wrap 
+                       top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         Swirl
       </span>
     </div>
-  );
+  )
 }
